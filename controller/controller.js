63e8c0b4213bc351d11ba55bc37bdaf5637ad098
Joseph Venuto <jbvenuto@gmail.com>
2019-05-20 12:36:14 -0400

    Removed old, commented out code

--- controller/controller.js
+++ controller/controller.js
@@ -8,42 +8,12 @@ router.get('/', (req, res) => {
 });
 
 router.get('/api/restaurants', (req, res) => {
-    // res.setHeader('Access-Control-Allow-Origin', '*')
     model.all(function(restaurantList) {
         res.json({
             data: restaurantList
         })
-        // res.render({
-        //     data: JSON.stringify(restaurantList)
-        // })
+        
     })
-
-    // let restaurantList = res.json({
-    //     data: model.all()
-    // }); 
-    // let restaurantList = model.all();
-    // console.log('req:', req);
-    // console.log('res:', res);
-
-    // console.log(`typeof restaurant list: ${typeof restaurantList}`);
-    // console.log(`controller restaurant list: ${restaurantList}`);
-    // console.log(model.all());
-    
-    // res.json(restaurantList);
-    
-    
-
-    // connection.query(selectRestaurants, (err, results) => {
-    //     if(err) {
-    //         return res.send(err)
-    //     }
-    //     else {
-    //         // console.log(results);
-    //         return res.json({
-    //             data: results
-    //         })
-    //     }
-    // });
 });
 
 router.get('/api/restaurants/:id', (req, res) => {
@@ -54,15 +24,6 @@ router.get('/api/restaurants/:id', (req, res) => {
 
     model.updateVisit(tempId);
     res.redirect("/api/restaurants");
-
-    // connection.query(`UPDATE restaurants SET visited = true WHERE id = ${tempId}`, (err, res) => {
-    //     if(err) {
-    //         return res.send(err)
-    //     }
-    //     else {
-    //         return res.sendStatus(200);
-    //     }
-    // });
 });
 
 router.get('/api/restaurants/rate/:id', (req, res) => {
@@ -74,34 +35,12 @@ router.get('/api/restaurants/rate/:id', (req, res) => {
     model.updateRating(tempId);
     res.redirect("/api/restaurants");
 
-    // connection.query(`UPDATE restaurants SET visitAgain = false WHERE id = ${tempId}`, (err, results) => {
-    //     if(err) {
-    //         return res.send(err)
-    //     }
-    //     else {
-    //         return res.sendStatus(200);
-    //     }
-    // });
 });
 
-//*********************************needs to be uncommented later************************************************** */
 router.post('/api/restaurants/create', bodyParser.urlencoded(), function(req, res) {
-//********************************************************************************************************** */
     let restaurantName = String(Object.keys(req.body));
-    // let sRestName = JSON.stringify(req.body);
-    // console.log(`Stingified restaurant name: ${sRestName}`);
 
     model.addRestaurant(restaurantName);
-
-    // connection.query(`INSERT INTO restaurants (name) VALUES ('${restaurantName}')`, (err, results) => {
-    //     if(err) {
-    //         console.log(err);
-    //         return res.send(err)
-    //     }
-    //     else {
-    //         return res.sendStatus(200);
-    //     }
-    // });
 });
 
 module.exports = router;
--- models/model.js
+++ models/model.js
@@ -5,40 +5,23 @@ const model = {
     // Get all of the restaurant data from the database
     all: (cb) => {
         connection.query('SELECT * FROM restaurants', (err, res) => {
-            // console.log(`res is ${res}`);
             if (err) {
                 throw err;
             }
             cb(res);
-            
-            // if(err) {
-            //     throw err
-            // }
-            // else {
-            //     console.log(results);
-            //     console.log(res);
-                
-            //     // return res.json({
-            //     //     data: res
-            //     // })
-            // }
         });
     },
 
     // Add a restaurant to the database
     addRestaurant: (restaurantName) => {
-        // let restaurantName = String(Object.keys(req.body));
-    // let sRestName = JSON.stringify(req.body);
-    // console.log(`Stingified restaurant name: ${sRestName}`);
+
 
         connection.query(`INSERT INTO restaurants (name) VALUES ('${restaurantName}')`, (err, results) => {
             if(err) {
                 console.log(err);
-                // return res.send(err)
                 throw err;
             }
             else {
-                // return res.sendStatus(200);
                 console.log('restaurant added');
             }
     });
@@ -46,19 +29,14 @@ const model = {
 
     // Update the visited status for a restaurant chosen by the user
     updateVisit: (tempId) => {
-        // let tempId = parseInt(req.params.id);
-        // console.log(req.params.id);
-        // console.log(Number.isInteger(req.params.id));
-        // console.log(Number.isInteger(tempId));
+
         console.log(`temptId in model: ${tempId}`);
 
         connection.query(`UPDATE restaurants SET visited = true WHERE id = ${tempId}`, (err, res) => {
             if(err) {
-                // return res.send(err)
                 throw err
             }
             else {
-            //     return res.sendStatus(200);
                 console.log('updated visited status');
             }
         });
@@ -66,17 +44,13 @@ const model = {
 
     // Update the rating for a restaurant after the user vists and does not want to go back
     updateRating: (tempId) => {
-        // let tempId = parseInt(req.params.id);
-        // console.log(req.params.id);
-        // console.log(Number.isInteger(req.params.id));
-        // console.log(Number.isInteger(tempId));
+
 
         connection.query(`UPDATE restaurants SET visitAgain = false WHERE id = ${tempId}`, (err, results) => {
             if(err) {
                 return res.send(err)
             }
             else {
-                // return res.sendStatus(200);
                 console.log('updated rating');
             }
         });
